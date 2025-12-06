829f0c0ce40971ee75ec5525fdb9555114d7352b
sujoyhalder815-ctrl <sujoyhalder815@gmail.com>
2025-12-05 21:55:07 -0500

    Implement counter functionality in main.js
    Added functionality to increment, decrement, and reset a counter.

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-4
+let count = 1;
+
+    const countDisplay = document.getElementById("count");
+    const decrementBtn = document.getElementById("decrement");
+    const resetBtn = document.getElementById("reset");
+    const incrementBtn = document.getElementById("increment");
+
+    const updateDisplay = () =>
+      (countDisplay.textContent = count);
+
+    const decrement = () => {
+      count--;
+      updateDisplay();
+    };
+
+    const increment = () => {
+      count++;
+      updateDisplay();
+    };
+
+    const reset = () => {
+      count = 0;
+      updateDisplay();
+    };
+
+
+    decrementBtn.addEventListener("click", decrement);
+    incrementBtn.addEventListener("click", increment);
+    resetBtn.addEventListener("click", reset);
+
